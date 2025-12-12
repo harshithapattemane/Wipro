7639ab28047650418bf07b1b3bf678fcd8008154
harshithapattemane <135418014+harshithapattemane@users.noreply.github.com>
2025-12-12 16:21:36 +0530

    Add files via upload

---
+++ day30/SpringSecurityDemo/src/main/java/com/example/SpringSecurityDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcomeMessage()
+	{
+		return "Home Page";
+	}
+
+}
